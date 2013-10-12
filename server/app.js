b99c4be5c171205ab8ff230c6bc2f5b974a539f7
chylvina@gmail.com <chylvina@gmail.com>
2013-10-13 00:37:28 +0800

    bugfix

--- server/app.js
+++ server/app.js
@@ -32,5 +32,5 @@ app.get('/', routes.index);
 app.post('/upload', routes.upload);
 
 app.listen(app.get('port'), function() {
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port " + app.get('port') + " in " + app.get('env') +  " mode.");
 });
